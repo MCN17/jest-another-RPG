b1c25172cc9de4bccec727294c6b75b630a6670c
Marty <martynolan17@gmail.com>
2022-07-27 14:25:57 -0400

    added a battle method

--- jest-another-RPG/lib/Enemy.js
+++ jest-another-RPG/lib/Enemy.js
@@ -34,11 +34,11 @@ Enemy.prototype.getHealth = function() {
     if (this.health < 0) {
       this.health = 0;
     }
-
-    Enemy.prototype.getDescription = function() {
-        return `A ${this.name} holding a ${this.weapon} has appeared!`;
-    };
   };
+
+  Enemy.prototype.getDescription = function() {
+    return `A ${this.name} holding a ${this.weapon} has appeared!`;
+};
 
 
 
